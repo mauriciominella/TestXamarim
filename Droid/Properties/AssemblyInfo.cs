ffa29b9bc750ee59430d9b79eca048e953a3aa8e
mauricio.minella@bravi.com.br <mauricio.minella@bravi.com.br>
2015-09-12 14:27:54 -0300

    Floating Action Button added to the application.

--- Droid/Properties/AssemblyInfo.cs
+++ Droid/Properties/AssemblyInfo.cs
@@ -13,7 +13,7 @@ using Android.App;
 [assembly: AssemblyCopyright ("Mauricio")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
-
+[assembly: Application(Theme = "@style/AppStyle.Light")]
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
---
+++ TestXamarim/Controls/FloatingActionButtonSize.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TestXamarim.Controls
+{
+	public enum FloatingActionButtonSize
+	{
+		Normal,
+		Mini
+	}
+}
+
